8c65ea71a09157ebc4e736e10463922c2fb4daba
PHIB16 <phib1_16@uni.worc.ac.uk>
2017-11-13 14:09:52 +0000

    beep

--- www/js/index.js
+++ www/js/index.js
@@ -6,6 +6,15 @@ $(document).on("pagecreate","#pageone",function(){
 
 	$('#taptext').on("tap",function(){
     	$(this).text(random);
+        
+        if(random == true){
+            navigator.notification.beep(1);
+           
+           }
+        else if(random == false){
+            navigator.notification.beep(2);
+            
+        }
  	}); 
 
 	 
